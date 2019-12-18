730355b122b4e4c4cb5acd00bd65514131fde936
Pedro Castilho <cstl.px@gmail.com>
2019-12-18 19:27:58 -0300

    Added position implementations

--- lib/position.ex
+++ lib/position.ex
@@ -20,7 +20,28 @@ defmodule Position do
     String.t
   ) :: {:ok, t} | {:no_parse, String.t}
   def from_strings(x_string, y_string, direction_string) do
-    :not_implemented
+    coordinate = Coordinate.from_strings(x_string, y_string)
+    direction = Direction.from_string(direction_string)
+    case {coordinate, direction} do
+      {{:ok, coords}, {:ok, dir}} -> {:ok, {coords, dir}}
+      _ ->
+        coord_error = get_error coordinate, fn data ->
+          "Invalid coordinates #{data}."
+        end
+        direction_error = get_error direction, fn data ->
+          "Invalid direction #{data}."
+        end
+        error = [coord_error, direction_error] |> Enum.join("\n")
+        {:no_parse, error}
+    end
+  end
+
+  defp get_error({err, data}, desc) do
+    if err != :ok do
+      "ERROR: " <> desc.(data)
+    else
+      ""
+    end
   end
 
   @doc """
@@ -28,8 +49,9 @@ defmodule Position do
   from that position.
   """
   @spec move_forward(t) :: t
-  def move_forward(position) do
-    :not_implemented
+  def move_forward(position)
+  def move_forward({coordinate, direction}) do
+    {Coordinate.move(coordinate, direction), direction}
   end
 
   @doc """
@@ -37,8 +59,9 @@ defmodule Position do
   from that position.
   """
   @spec turn_left(t) :: t
-  def turn_left(position) do
-    :not_implemented
+  def turn_left(position)
+  def turn_left({coordinate, direction}) do
+    {coordinate, Direction.turn_left(direction)}
   end
 
   @doc """
@@ -46,7 +69,8 @@ defmodule Position do
   from that position.
   """
   @spec turn_right(t) :: t
-  def turn_right(position) do
-    :not_implemented
+  def turn_right(position)
+  def turn_right({coordinate, direction}) do
+    {coordinate, Direction.turn_right(direction)}
   end
 end
