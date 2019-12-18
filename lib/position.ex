e7849e485e5673a717b0bdb9187a0ea21729fc46
Pedro Castilho <cstl.px@gmail.com>
2019-12-18 19:07:56 -0300

    Added position unit tests

--- lib/position.ex
+++ lib/position.ex
@@ -8,4 +8,45 @@ defmodule Position do
   and direction. Use as `Position.t`.
   """
   @type t :: {Coordinate.t, Direction.t}
+
+  @doc """
+  Takes three strings representing the `x_coordinate`, `y_coordinate`
+  and `direction` of the probe and returns either :ok and the specified
+  position or :no_parse and a description of what went wrong.
+  """
+  @spec from_strings(
+    String.t,
+    String.t,
+    String.t
+  ) :: {:ok, t} | {:no_parse, String.t}
+  def from_strings(x_string, y_string, direction_string) do
+    :not_implemented
+  end
+
+  @doc """
+  Takes a `position` and returns the position obtained by moving forward
+  from that position.
+  """
+  @spec move_forward(t) :: t
+  def move_forward(position) do
+    :not_implemented
+  end
+
+  @doc """
+  Takes a `position` and returns the position obtained by turning left
+  from that position.
+  """
+  @spec turn_left(t) :: t
+  def turn_left(position) do
+    :not_implemented
+  end
+
+  @doc """
+  Takes a `position` and returns the position obtained by turning right
+  from that position.
+  """
+  @spec turn_right(t) :: t
+  def turn_right(position) do
+    :not_implemented
+  end
 end
---
+++ test/position_test.exs
@@ -0,0 +1,45 @@
+defmodule PositionTest do
+  @moduledoc """
+  Unit tests for functions that operate on the probe's position.
+  """
+
+  use ExUnit.Case
+  doctest Position
+
+  test "Parse valid options returns position" do
+    assert Position.from_strings("0", "0", "N") == {:ok, {{0, 0}, :N}}
+    assert Position.from_strings("1", "-1", "S") == {:ok, {{1, -1}, :S}}
+  end
+
+  test "Parse invalid options returns error" do
+    # We do not specify the error messages here. The Position module should
+    # emit user-facing error messages which should be able to change for many
+    # reasons.
+    assert Position.from_strings("A", "B", "C")  |> elem(0) == :no_parse
+    assert Position.from_strings("0", "0", "Z")  |> elem(0) == :no_parse
+    assert Position.from_strings("0Y", "0", "S") |> elem(0) == :no_parse
+    assert Position.from_strings("0", "0X", "E") |> elem(0) == :no_parse
+    assert Position.from_strings(:"0", :"0", :N) |> elem(0) == :no_parse
+  end
+
+  test "Move forward moves the probe along current direction" do
+    assert Position.move_forward({{0, 0}, :N}) == {{0, 1}, :N}
+    assert Position.move_forward({{0, 0}, :S}) == {{0, -1}, :S}
+    assert Position.move_forward({{0, 0}, :E}) == {{1, 0}, :E}
+    assert Position.move_forward({{0, 0}, :W}) == {{-1, 0}, :W}
+  end
+
+  test "Turn left changes the current direction to the left once" do
+    assert Position.turn_left({{0, 0}, :N}) == {{0, 0}, :W}
+    assert Position.turn_left({{0, 0}, :E}) == {{0, 0}, :N}
+    assert Position.turn_left({{0, 0}, :W}) == {{0, 0}, :S}
+    assert Position.turn_left({{0, 0}, :S}) == {{0, 0}, :E}
+  end
+
+  test "Turn right changes the current direction to the right once" do
+    assert Position.turn_right({{0, 0}, :N}) == {{0, 0}, :E}
+    assert Position.turn_right({{0, 0}, :E}) == {{0, 0}, :S}
+    assert Position.turn_right({{0, 0}, :W}) == {{0, 0}, :N}
+    assert Position.turn_right({{0, 0}, :S}) == {{0, 0}, :W}
+  end
+end
