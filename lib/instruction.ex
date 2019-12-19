96d3843101407b4b2409e438bb47c10fcbe1c7d3
Pedro Castilho <cstl.px@gmail.com>
2019-12-19 19:52:03 -0300

    Removed `Probe` module

--- lib/instruction.ex
+++ lib/instruction.ex
@@ -1,6 +1,9 @@
 defmodule Instruction do
   @moduledoc """
   This module defines functions that operate on the probe's instructions.
+
+  This module - and only this module - should change if new instructions are
+  added, instructions are removed or if the meaning of instructions is changed.
   """
 
   @typedoc """
@@ -19,6 +22,23 @@ defmodule Instruction do
       "R" -> :R
       "M" -> :M
       _   -> {:no_parse, string}
+    end
+  end
+
+  @doc """
+  Takes a probe position and an instruction and returns the next position of the
+  probe.
+  """
+  @spec run(
+    Position.t,
+    Instruction.t
+  ) :: Position.t | :invalid_instruction
+  def run(position, instruction) do
+    case instruction do
+      :L -> Position.turn_left(position)
+      :R -> Position.turn_right(position)
+      :M -> Position.move_forward(position)
+      _  -> :invalid_instruction
     end
   end
 end
--- test/instruction_test.exs
+++ test/instruction_test.exs
@@ -18,4 +18,18 @@ defmodule InstructionTest do
     assert Instruction.from_string("01") == {:no_parse, "01"}
     assert Instruction.from_string(nil) == {:no_parse, nil}
   end
+
+  test "Running instructions works correctly" do
+    origin = {0, 0}
+    assert Instruction.run({origin, :N}, :L) == {origin, :W}
+    assert Instruction.run({origin, :N}, :R) == {origin, :E}
+    assert Instruction.run({origin, :N}, :M) == {{0, 1}, :N}
+  end
+
+  test "Running invalid instruction returns atom" do
+    origin = {0, 0}
+    assert Instruction.run({origin, :N}, :T) == :invalid_instruction
+    assert Instruction.run({origin, :N}, 0) == :invalid_instruction
+    assert Instruction.run({origin, :N}, nil) == :invalid_instruction
+  end
 end
